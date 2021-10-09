7dd978926db354774cf48e525b8f45d7c7049590
Сергей Куцев <sergeikutsev@yandex.ru>
2021-10-10 00:44:04 +0300

    ОК

---
+++ Homework4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ввести число N. Найти квадраты всех чисел от 1 до числа N по формуле N^2=1 + 3 + 5 + ... + (2*N – 1). 
+             После добавления к сумме каждого слагаемого выводить текущее значение суммы */
+
+
+            Console.WriteLine("Введите значение N");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for ( int N4 = 1; N4 < N ; N4++)
+            {
+                int N2 = N4 * N4;
+                int N3 = N4 * N4 - (2 * N4 - 1);
+                int N5 = (2 * N4 - 1);
+                Console.WriteLine("Квадрат числа {0} = {1} + 2 * {2} - 1 = {3}", N4, N3, N4, N2 = N3 + (2 * N4 - 1));
+            }
+
+            //for (int N6 = 0; N6 <= N3 ; N3 +=2)
+
+
+                Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Task2_DoWhile/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Осуществить ввод последовательности целых чисел и сравнить, что больше, количество положительных или количество отрицательных.
+            Последовательность потенциально не ограничена, окончанием последовательности служит число 0.
+            */
+
+
+            int Number;
+            int negativeNumber = 0;
+            int positiveNumber = 0;
+            do
+            {
+                Number = Convert.ToInt16(Console.ReadLine());
+
+                int mNumber = Math.Abs(Number);
+                int x = Number*mNumber;
+
+
+               int  v = (x < 0) ? negativeNumber++:0;
+               int y = (x > 0) ? positiveNumber++:0;
+
+            } while (Number != 0);
+
+            if (negativeNumber > positiveNumber)
+            {
+                Console.WriteLine("Отрицательных чисел равно {0} и их больше чем положительных {1}.", negativeNumber, positiveNumber);
+            }
+            else if (positiveNumber > negativeNumber)
+            {
+                Console.WriteLine("Положительных чисел равно {1} и их больше чем отрицательных {0}.", negativeNumber, positiveNumber);
+            }
+            else
+            {
+                Console.WriteLine("Количество отрицательных чисел равно количеству положительных чисел и их общее количество равняется {0}.", negativeNumber + positiveNumber);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
